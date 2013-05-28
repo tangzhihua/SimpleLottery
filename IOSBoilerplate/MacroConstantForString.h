db0f748e89846a856068543bab2bf37fe3738848
tangzhihua <3252475@qq.com>
2013-05-28 15:20:40 +0800

    代码整理

--- IOSBoilerplate/MacroConstantForString.h
+++ IOSBoilerplate/MacroConstantForString.h
@@ -9,6 +9,7 @@
 #ifndef airizu_MacroConstant_h
 #define airizu_MacroConstant_h
 
+// 客服电话
 #define kCustomerServicePhoneNumber NSLocalizedString(@"400-890-2020", @"客服电话:400-890-2020")
 // 传入的 Intent 无效
 #define kIncomingIntentValid NSLocalizedString(@"Incoming intent invalid", @"Activity之间的通信载体Intent对象无效")
--- IOSBoilerplate/MacroConstantForThisProject.h
+++ IOSBoilerplate/MacroConstantForThisProject.h
@@ -64,11 +64,11 @@ typedef NS_ENUM(NSInteger, LotteryOpenPrizeStatusEnum) {
 
 #define kCoopid   @"28" //appStore
 
-//#define kRuYiCaiCoopid   @"29" //91手机助手iPhone
-//#define kRuYiCaiCoopid   @"34" //泡椒iPhone
-//#define kRuYiCaiCoopid   @"35" //蚕豆网iPhone
-//#define kRuYiCaiCoopid   @"49" //十字猫
-//#define kRuYiCaiCoopid   @"53" //软吧
+//#define kRuYiCaiCoopid   @"29" // 91手机助手iPhone
+//#define kRuYiCaiCoopid   @"34" // 泡椒iPhone
+//#define kRuYiCaiCoopid   @"35" // 蚕豆网iPhone
+//#define kRuYiCaiCoopid   @"49" // 十字猫
+//#define kRuYiCaiCoopid   @"53" // 软吧
 
 #define kAESKey @"<>hj12@#$$%^~~ff"
 
@@ -76,63 +76,77 @@ typedef NS_ENUM(NSInteger, LotteryOpenPrizeStatusEnum) {
 #define kAdImageNameForWelcomePage @"adimage.png"
 
 
-//
+// 七乐彩
 #define kLotteryKey_7lecai                  @"7lecai"
 #define kLotteryCode_7lecai                 @"F47102"
 #define kLotteryName_7lecai                 @"七乐彩"
-//
+
+// 七星彩
 #define kLotteryKey_7xingcai                @"7xingcai"
 #define kLotteryCode_7xingcai               @"T01009"
 #define kLotteryName_7xingcai               @"七星彩"
-//
+
+// 22选5
 #define kLotteryKey_22xuan5                 @"22xuan5"
 #define kLotteryCode_22xuan5                @"T01013"
 #define kLotteryName_22xuan5                @"22选5"
-//
+
+// 大乐透
 #define kLotteryKey_daletou                 @"daletou"
 #define kLotteryCode_daletou                @"T01001"
 #define kLotteryName_daletou                @"大乐透"
-//
+
+// 福彩3D
 #define kLotteryKey_fucai_3d                @"fucai_3d"
 #define kLotteryCode_fucai_3d               @"F47103"
 #define kLotteryName_fucai_3d               @"福彩3D"
-//
+
+// 广东11选5
 #define kLotteryKey_guangdong_11xuan5       @"guangdong_11xuan5"
 #define kLotteryCode_guangdong_11xuan5      @"T01014"
 #define kLotteryName_guangdong_11xuan5      @"广东11选5"
-//
-#define kLotteryKey_jingcai_basketball     @"jingcai_basketball"
-#define kLotteryCode_jingcai_basketball    @"JC_L"
-#define kLotteryName_jingcai_basketball    @"竞彩篮球"
-//
+
+// 竞彩篮球
+#define kLotteryKey_jingcai_basketball      @"jingcai_basketball"
+#define kLotteryCode_jingcai_basketball     @"JC_L"
+#define kLotteryName_jingcai_basketball     @"竞彩篮球"
+
+// 江西11选5
 #define kLotteryKey_jiangxi_11xuan5         @"jiangxi_11xuan5"
 #define kLotteryCode_jiangxi_11xuan5        @"T01010"
 #define kLotteryName_jiangxi_11xuan5        @"江西11选5"
-//
+
+// 竞彩足球
 #define kLotteryKey_jingcai_football        @"jingcai_football"
 #define kLotteryCode_jingcai_football       @"JC_Z"
 #define kLotteryName_jingcai_football       @"竞彩足球"
-//
+
+// 排列三
 #define kLotteryKey_pailie3                 @"pailie3"
 #define kLotteryCode_pailie3                @"T01002"
 #define kLotteryName_pailie3                @"排列三"
-//
+
+// 排列五
 #define kLotteryKey_pailie5                 @"pailie5"
 #define kLotteryCode_pailie5                @"T01001"
 #define kLotteryName_pailie5                @"排列五"
-//
+
+// 山东十一运夺金
 #define kLotteryKey_shandong_11yun_duojin   @"shandong_11yun_duojin"
 #define kLotteryCode_shandong_11yun_duojin  @"T01012"
 #define kLotteryName_shandong_11yun_duojin  @"山东十一运夺金"
-//
+
+// 时时彩
 #define kLotteryKey_shishicai               @"shishicai"
 #define kLotteryCode_shishicai              @"T01007"
 #define kLotteryName_shishicai              @"时时彩"
-//
+
+// 双色球
 #define kLotteryKey_shuangseqiu             @"shuangseqiu"
 #define kLotteryCode_shuangseqiu            @"F47104"
 #define kLotteryName_shuangseqiu            @"双色球"
-//
+
+// 足彩
 #define kLotteryKey_zucai                   @"zucai"
 #define kLotteryCode_zucai                  @"T01003"
 #define kLotteryName_zucai                  @"足彩"

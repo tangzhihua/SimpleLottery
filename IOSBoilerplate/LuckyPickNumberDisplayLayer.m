e41c078c62e6ab5a24004cb96b430ed7c89f758d
tangzhihua <3252475@qq.com>
2013-06-04 16:59:26 +0800

    完成 cocos2d 的一个简单demo

--- IOSBoilerplate/LuckyPickNumberDisplayLayer.m
+++ IOSBoilerplate/LuckyPickNumberDisplayLayer.m
@@ -27,17 +27,17 @@
       UIGraphicsBeginImageContextWithOptions(view.frame.size, NO, 0.0);
     } else {
       UIGraphicsBeginImageContext(view.frame.size);
-    }  
-
+    }
+    
     [view.layer renderInContext:UIGraphicsGetCurrentContext()];
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-
+    
 		CCSprite *background;
 		
 		background = [CCSprite spriteWithCGImage:image.CGImage key:@"test"];
-     
-		background.position = ccp(0, size.height+100);
+    
+		background.position = ccp(160, size.height+170);
     
 		// add the label as a child to this Layer
 		[self addChild: background];
@@ -45,7 +45,7 @@
     // Default font size will be 28 points.
 		[CCMenuItemFont setFontSize:28];
 		
- 
+    
 		
 		// Achievement Menu Item using blocks
 		CCMenuItem *menuItemOfGo = [CCMenuItemFont itemWithString:@"关闭" block:^(id sender) {
@@ -56,11 +56,11 @@
       //	id move_back = [move reverse];
       
       id move_ease_inout1 = [CCEaseElasticInOut actionWithAction:[move copy] period:0.3f];
-    
+      
       
       
 			[self runAction:move_ease_inout1];
-
+      
       
 		}];
 		
@@ -71,11 +71,11 @@
 		
     [menu setColor:ccBLUE];
 		[menu alignItemsHorizontallyWithPadding:40];
-		[menu setPosition:ccp(size.width/2, size.height+100)];
+		[menu setPosition:ccp(size.width/2, size.height+300)];
 		
 		// Add the menu to the layer
 		[self addChild:menu];
-
+    
 	}
 	
 	return self;

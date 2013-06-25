b841c7a82adcf8f0284186447b000428c7924361
tangzhihua <3252475@qq.com>
2013-06-25 16:09:38 +0800

    UIAlertView+Blocks.h 修改成可以商用的代码

--- IOSBoilerplate/Category/UIAlertView+Blocks.h
+++ IOSBoilerplate/Category/UIAlertView+Blocks.h
@@ -9,13 +9,7 @@ typedef void (^DismissBlock)(int buttonIndex);
 typedef void (^CancelBlock)();
 
 
-// 20130625 : 这个类别其实实现的有问题, 因为DismissBlock和CancelBlock块不会跟每个UIAlertView准确对应
-// 如果外面连续两次调用 showAlertViewWithTitle 那么只是最后的 DismissBlock和CancelBlock块 有效.
-// 也就是说这个类别方法只适用于同一时间只显示一个UIAlertView的情况下, 才有有意义, 如果第一个UIAlertView弹出后,
-// 用户还未做出选择, 此时第二个弹出框也弹出了, 那么DismissBlock和CancelBlock块 就只是第二个UIAlertView的块了
-
-
-// 这里只是为了演示 过程式编程 和 函数式编程
+// 这里为了演示 过程式编程 和 函数式编程
 // 函数式编程中, 代码读起来更像人类语言.
 @interface UIAlertView (Blocks) <UIAlertViewDelegate>
 
--- IOSBoilerplate/Category/UIAlertView+Blocks.m
+++ IOSBoilerplate/Category/UIAlertView+Blocks.m
@@ -5,11 +5,18 @@
 
 #import "UIAlertView+Blocks.h"
 
-static DismissBlock _dismissBlock;
-static CancelBlock _cancelBlock;
+// 因为只能在UI线程中调用 UIAlertView, 所以这里应该不需要考虑 同步点
+static NSMutableDictionary *blockCache(){
+  static NSMutableDictionary *_blockCache = nil;
+  if (_blockCache == nil) {
+    _blockCache = [NSMutableDictionary dictionaryWithCapacity:50];
+  }
+  return _blockCache;
+}
 
 @implementation UIAlertView (Blocks)
 
+static NSInteger _tagForUIAlertView = 0;
 + (UIAlertView *) showAlertViewWithTitle:(NSString *) title
                                  message:(NSString *) message
                        cancelButtonTitle:(NSString *) cancelButtonTitle
@@ -17,15 +24,17 @@ static CancelBlock _cancelBlock;
                                onDismiss:(DismissBlock) dismissed
                                 onCancel:(CancelBlock) cancelled {
   
-  _cancelBlock  = [cancelled copy];
-  
-  _dismissBlock  = [dismissed copy];
-  
   UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
                                                   message:message
                                                  delegate:[self self]
                                         cancelButtonTitle:cancelButtonTitle
                                         otherButtonTitles:nil];
+  
+  alert.tag = _tagForUIAlertView++;
+  NSMutableArray *blockArray = [NSMutableArray arrayWithCapacity:2];
+  blockArray[0] = cancelled;
+  blockArray[1] = dismissed;
+  [blockCache() setObject:blockArray forKey:[NSNumber numberWithInteger:alert.tag]];
   
   for(NSString *buttonTitle in otherButtons) {
     [alert addButtonWithTitle:buttonTitle];
@@ -35,13 +44,19 @@ static CancelBlock _cancelBlock;
   return alert;
 }
 
-+ (void)alertView:(UIAlertView*) alertView didDismissWithButtonIndex:(NSInteger) buttonIndex {
++ (void) alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger) buttonIndex {
+  
+  NSMutableArray *blockArray = [blockCache() objectForKey:[NSNumber numberWithInteger:alertView.tag]];
+  CancelBlock cancelBlock = blockArray[0];
+  DismissBlock dismissBlock = blockArray[1];
   
 	if(buttonIndex == [alertView cancelButtonIndex]) {
-		_cancelBlock();
+		cancelBlock();
 	} else {
-    _dismissBlock(buttonIndex - 1); // cancel button is button 0
+    dismissBlock(buttonIndex - 1); // cancel button is button 0
   }
+  
+  [blockCache() removeObjectForKey:[NSNumber numberWithInteger:alertView.tag]];
 }
 
 

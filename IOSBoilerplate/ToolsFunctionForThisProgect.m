52e552a17799926cc0ddc58303cf6b48d80158f3
tangzhihua <3252475@qq.com>
2013-09-29 10:02:52 +0800

    限定 ToolsFunctionForThisProgect , 让外部不能对其进行 创建对象的功能.

--- IOSBoilerplate/ToolsFunctionForThisProgect.m
+++ IOSBoilerplate/ToolsFunctionForThisProgect.m
@@ -29,6 +29,14 @@
 
 @implementation ToolsFunctionForThisProgect
 
+#pragma mark
+#pragma mark 不能使用默认的init方法初始化对象, 而必须使用当前类特定的 "初始化方法" 初始化所有参数
+- (id) init {
+  RNAssert(NO, @"Can not use the default init method!");
+  
+  return nil;
+}
+
 /**
  * 记录用户登录成功后的重要信息
  *
